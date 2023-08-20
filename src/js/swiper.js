d0cac32afa96d37ffcc7091e0296a05b4b7269a0
Oksana Zakharchuk <126189342+OksaZa@users.noreply.github.com>
2023-08-20 11:35:20 +0300

    Hero
    fix Swiper

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -2,10 +2,29 @@ const swiper = new Swiper('.swiper', {
   // Optional parameters
   direction: 'horizontal',
   loop: true,
+  speed: 700,
+  effect: 'cube',
+  cubeEffect: {
+    slideShadows: true,
+    shadows: true,
+    shadowOffset: 20,
+    shadowScale: 0.9,
+  },
 
   // If we need pagination
   pagination: {
     el: '.swiper-pagination',
+    type: 'fraction',
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' +
+              '<span class="swiper-pagination-decor"> - </span>' +
+              '<span class="' + totalClass + '"></span>';
+    }
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
   },
 
   // Navigation arrows
@@ -13,9 +32,6 @@ const swiper = new Swiper('.swiper', {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
-  },
 });
+
+swiper.autoplay.start();
